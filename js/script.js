d88e645cf346686b03708e671d91e37b921c1023
Herzy-Muthaqin <herzymuthaqin007@gmail.com>
2022-11-20 19:43:11 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert('Selamat Datang di Website Saya');
+
+alert('Selamat Menikmati');
+
+function like() {
+    alert("Makasih");
+}
+
+function donate() {
+    alert("Terima kasih sudah berdonasi")
+}
+
+function dislike() {
+    alert("HUHUU Jadi Sedihh T_T");
+}
